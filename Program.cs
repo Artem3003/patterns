e31bca442e12353a2e8be98c005d0742f8a0536f
Artem <temapro04@gmai.com>
2023-07-27 22:45:46 +0300

    Factory method base info

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Combos combos = ComboCreator.CreateCombos(ComboCode.LITE);
+            combos.PrintOrder();
+            
+            Combos combos1 = ComboCreator.CreateCombos(ComboCode.FAMILY);
+            combos.PrintOrder();
+
+            Combos combos2 = ComboCreator.CreateCombos(ComboCode.MEGA);
+            combos.PrintOrder();
+        }
+    }
+}
