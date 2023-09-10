66bad23d545f1df8581a0c66886ad02c9e5a12b6
Artem <temapro04@gmai.com>
2023-09-10 22:07:33 +0300

    Conceptual example of Abstract Factory pattern

--- creational/abstract facrory/AbstractFactory.cs
+++ creational/abstract facrory/AbstractFactory.cs
@@ -2,8 +2,139 @@ using System;
 
 namespace patterns
 {
-    class AbstractFactory
+    // The Abstract Factory interface declares a set of methods that return
+    // different abstract products. These products are called a family and are
+    // related by a high-level theme or concept. Products of one family are
+    // usually able to collaborate among themselves. A family of products may
+    // have several variants, but the products of one variant are incompatible
+    // with products of another.
+    public interface IAbstractFactory
     {
-        
+        IAbstractProductA CreateProductA();
+
+        IAbstractProductB CreateProductB();
+    }
+
+    // Concrete Factories produce a family of products that belong to a single
+    // variant. The factory guarantees that resulting products are compatible.
+    // Note that signatures of the Concrete Factory's methods return an abstract
+    // product, while inside the method a concrete product is instantiated.
+    class ConcreateFactory1 : IAbstractFactory
+    {
+        public IAbstractProductA CreateProductA()
+        {
+            return new ConcreteProductA1();
+        }
+
+        public IAbstractProductB CreateProductB()
+        {
+            return new ConcreteProductB1();
+        }
+    }
+
+    class ConcreateFactory2 : IAbstractFactory 
+    {
+        public IAbstractProductA CreateProductA()
+        {
+            return new ConcreteProductA2();
+        }
+
+        public IAbstractProductB CreateProductB()
+        {
+            return new ConcreteProductB2();
+        }
+    }
+
+    // Each distinct product of a product family should have a base interface.
+    // All variants of the product must implement this interface.
+
+    public interface IAbstractProductA
+    {
+        string UsefulFunctionA();
+    }
+
+    // Concrete Products are created by corresponding Concrete Factories.
+    
+    class ConcreteProductA1 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "The result of the product A1.";
+        }
+    } 
+
+    class ConcreteProductA2 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "The result of the product A2.";
+        }
+    }
+
+    // Here's the the base interface of another product. All products can
+    // interact with each other, but proper interaction is possible only between
+    // products of the same concrete variant.
+    public interface IAbstractProductB
+    {
+        // Product B is able to do its own thing...
+        string UsefulFunctionB();
+
+        // ...but it also can collaborate with the ProductA.
+        //
+        // The Abstract Factory makes sure that all products it creates are of
+        // the same variant and thus, compatible.
+        string AnotherUsefulFunctionB(IAbstractProductA collaborator);
+    }
+
+    class ConcreteProductB1 : IAbstractProductB
+    {
+        public string UsefulFunctionB()
+        {
+            return "The result of the product B1.";
+        }
+
+        public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+        {
+            var result = collaborator.UsefulFunctionA();
+
+            return $"The result of the B1 collaborating with the ({result})";
+        }
+    }
+
+    class ConcreteProductB2 : IAbstractProductB
+    {
+        public string UsefulFunctionB()
+        {
+            return "The result of the product B2.";
+        }
+
+        public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+        {
+            var result = collaborator.UsefulFunctionA();
+
+            return $"The result of the B2 collaborating with the ({result})";
+        }
+    }
+
+    class Client
+    {
+        public void Main()
+        {
+            System.Console.WriteLine("Client: Testing client code with the first factory type...");
+            ClientMethod(new ConcreateFactory1());
+            System.Console.WriteLine();
+
+            System.Console.WriteLine("Client: Testing the same client code with the second factory type...");
+            ClientMethod(new ConcreateFactory2());
+        }
+
+        public void ClientMethod(IAbstractFactory factory)
+        {
+            IAbstractProductA productA = factory.CreateProductA();
+            IAbstractProductB productB = factory.CreateProductB();
+
+            System.Console.WriteLine(productB.UsefulFunctionB());
+            System.Console.WriteLine(productB.AnotherUsefulFunctionB(productA));
+        }
     }
 }
--- creational/abstract facrory/README.md
+++ creational/abstract facrory/README.md
@@ -1,3 +1,11 @@
 ## Abstract factory 
 
 The abstract factory pattern is a creational pattern that provides a way to encapsulate a group of individual factories that have a common theme without specifying their concrete classes. Abstract Factory pattern is almost similar to Factory Pattern and is considered as another layer of abstraction over factory pattern. 
+
+## Usage exaples 
+
+Many frameworks and libraries use it to provide a way to extend and costomize their standard components.
+
+## Identification
+
+The pattern is easy to recognize by methods, which return a factory object. Then, the factory is used for creating specific sub-components.
