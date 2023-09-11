b7e97abf8bec3e72589d56c0fa62cb2da291a684
Artem <temapro04@gmai.com>
2023-09-11 22:25:05 +0300

    Prototype base info

---
+++ creational/prototype/Prototype.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace patterns
+{
+    public class Person
+    {
+        public int Age;
+        public DateTime BirthDate;
+        public string Name;
+        public IdInfo IdInfo;
+
+        public Person ShallowCopy()
+        {
+            return (Person) this.MemberwiseClone();
+        }
+
+        public Person DeepCopy()
+        {
+            Person other = (Person) this.MemberwiseClone();
+            other.IdInfo = new IdInfo(IdInfo.IdNumber);
+            other.Name = String.Copy(Name);
+            return other;
+        }
+    }
+
+    public class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int idNumber)
+        {
+            this.IdNumber = idNumber;
+        }
+    }
+}
